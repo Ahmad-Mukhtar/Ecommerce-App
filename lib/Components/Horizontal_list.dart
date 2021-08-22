b708ff77df1d24469f915eea41ecf4b43c1b41a6
Blank <gracenoteii5@gmail.com>
2021-08-22 16:23:27 +0500

    Ecommerce App In progress

---
+++ lib/Components/Horizontal_list.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HorizontalList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+        Category(image_caption: 'Shirts', image_location: 'images/cats/tshirt.png'),
+          Category(image_caption: 'Dress', image_location: 'images/cats/dress.png'),
+          Category(image_caption: 'Formal', image_location: 'images/cats/formal.png'),
+        /*  Category(image_caption: 'Informal', image_location: 'images/cats/informal.png'),
+          Category(image_caption: 'Jeans', image_location: 'images/cats/jeans.png'),
+          Category(image_caption: 'Shoes', image_location: 'images/cats/shoe.png'),
+          Category(image_caption: 'Accesssories', image_location: 'images/cats/accessories.png'),
+          */
+        ],
+      ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+
+  late final String image_location;
+  late final String image_caption;
+
+  Category({required this.image_caption, required this.image_location
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: EdgeInsets.all(2.0),
+      child: InkWell(
+        onTap: (){},
+        child: Container(
+          width: 100.0,
+          child: ListTile(
+            title: Image.asset(image_location,width: 100.0,height: 80.0),
+            subtitle: Text(image_caption,textAlign: TextAlign.center,),
+          ),
+        ),
+    ),
+    );
+  }
+}
---
+++ lib/Components/Products.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Products extends StatefulWidget {
+  @override
+  _ProductsState createState() => _ProductsState();
+}
+
+class _ProductsState extends State<Products> {
+
+  var product_list=[
+    {
+      "name":"Balzer",
+      "picture":"images/products/blazer1.jpeg",
+      "old_price":120,
+      "price":85
+    },
+    {
+      "name":"Hills",
+      "picture":"images/products/hills1.jpeg",
+      "old_price":130,
+      "price":95
+    },
+    {
+      "name":"Black Dress",
+      "picture":"images/products/dress2.jpeg",
+      "old_price":150,
+      "price":55
+    },
+    {
+      "name":"Pants",
+      "picture":"images/products/pants1.jpg",
+      "old_price":120,
+      "price":65
+    },
+    {
+      "name":"Shoe",
+      "picture":"images/products/shoe1.jpg",
+      "old_price":190,
+      "price":175
+    },
+    {
+      "name":"Skirt",
+      "picture":"images/products/skt2.jpeg",
+      "old_price":160,
+      "price":125
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: product_list.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context,int index){
+          return Singleproducts(
+            product_name: product_list[index]['name'],
+            product_picture: product_list[index]['picture'],
+            old_price: product_list[index]['old_price'],
+            price: product_list[index]['price']
+          );
+        },);
+  }
+}
+
+class Singleproducts extends StatelessWidget {
+
+  late final product_name;
+  late final  product_picture;
+  late final price;
+  late final old_price;
+
+  Singleproducts({
+   this.product_name,
+   this.product_picture,
+   this.old_price,
+   this.price
+});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Hero(
+        tag: product_name,
+        child: Material(
+
+          child: InkWell(onTap: (){
+            print("Tapped "+ product_name);
+          },
+          child: GridTile(
+            footer: Container(
+              color: Colors.white70,
+              child: ListTile(
+                leading: Text(product_name,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),
+              ),
+            title: Text("\$$price",textAlign: TextAlign.end,style: TextStyle(fontWeight: FontWeight.bold),),
+              subtitle: Text("\$$old_price",textAlign: TextAlign.end,style: TextStyle(fontWeight: FontWeight.bold,decoration: TextDecoration.lineThrough)),
+              ),
+          ),
+    child: Image.asset(
+    product_picture,
+    fit: BoxFit.cover,
+    ),
+          ),
+        ),
+      ),
+    ));
+  }
+}
